4fbd509e087fc4d9c3493a941b51a53734835b2d
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-05-21 17:35:40 -0500

    Agregada randomizacion

--- sketch.js
+++ sketch.js
@@ -5,17 +5,17 @@ function setup() {
 function draw() {
   background(220);
   fill (255,0,0);
-  ellipse (50,50,50,50);
+  ellipse (random(0,100),50,50,50);
 
   fill (255,255,0);
-  ellipse (50,150,50,50);
+  ellipse (random(0,100),150,50,50);
 
   fill (0);
   textAllign(CENTER);
   text("Nicolas Orozco,Sebastian Mosquera,Natalia Martinez",70,50);
  
   fill (0,255,0);
-  ellipse (150,150,50,50);
+  ellipse (random(0,100),150,50,50);
   
   text("b",80,150);
 
